c042d97d7e61c629a2fbc081fd0a2e9c83e34990
Dietrich Epp <depp@zdome.net>
2019-10-22 14:14:07 -0400

    Draw triangle on screen

--- tcm/main.c
+++ tcm/main.c
@@ -1,17 +1,116 @@
 // OpenGL is deprecated on macOS. We don't care. This silences the warnings.
 #define GL_SILENCE_DEPRECATION 1
 
+// To avoid conflict when we define these twice.
+#define GLFW_INCLUDE_NONE
+
+#include <OpenGL/gl3.h>
+
 #include <GLFW/glfw3.h>
 
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static void die(const char *msg) __attribute__((noreturn));
 
 static void die(const char *msg) {
     fprintf(stderr, "Error: %s\n", msg);
     exit(1);
+}
+
+static const char VERT_SHADER[] =
+    "#version 330\n"
+    "layout(location = 0) in vec2 in_pos;\n"
+    "out VertexData {\n"
+    "    vec3 color;\n"
+    "} o;\n"
+    "void main() {\n"
+    "    o.color = vec3(in_pos.xy, 0.0);\n"
+    "    gl_Position = vec4(in_pos, 0.0, 1.0);\n"
+    "}\n";
+
+static const char FRAG_SHADER[] =
+    "#version 330\n"
+    "in VertexData {\n"
+    "    vec3 color;\n"
+    "} i;\n"
+    "out vec4 out_color;\n"
+    "void main() {\n"
+    "    out_color = vec4(i.color, 0.0);\n"
+    "}\n";
+
+// Load a single shader, and return the shader object.
+static GLuint load_shader(GLenum type, const char *source) {
+    GLuint shader = glCreateShader(type);
+    if (shader == 0) {
+        die("Could not create shader");
+    }
+    glShaderSource(shader, 1, (const char *[]){source},
+                   (const GLint[]){strlen(source)});
+    glCompileShader(shader);
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (!status) {
+        fputs("Error: Could not compile shader.\n", stderr);
+    }
+    GLint loglen;
+    // loglen includes nul terminator.
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &loglen);
+    if (loglen > 1) {
+        char *log = malloc(loglen);
+        if (log == NULL) {
+            die("No memory");
+        }
+        glGetShaderInfoLog(shader, loglen, NULL, log);
+        log[loglen - 1] = '\0';
+        fputs("-- Shader compilation log --\n", stderr);
+        fputs(log, stderr);
+        free(log);
+    }
+    if (!status) {
+        die("Shader compilation failed.");
+    }
+    return shader;
+}
+
+// Load the shader program and return the program object.
+static GLuint load_shaders(void) {
+    GLuint vert = load_shader(GL_VERTEX_SHADER, VERT_SHADER);
+    GLuint frag = load_shader(GL_FRAGMENT_SHADER, FRAG_SHADER);
+    GLuint prog = glCreateProgram();
+    if (prog == 0) {
+        die("Could not create program");
+    }
+    glAttachShader(prog, vert);
+    glAttachShader(prog, frag);
+    glDeleteShader(vert);
+    glDeleteShader(frag);
+    glLinkProgram(prog);
+    GLint status;
+    glGetProgramiv(prog, GL_LINK_STATUS, &status);
+    if (!status) {
+        fputs("Error: Could not link program.\n", stderr);
+    }
+    GLint loglen;
+    // loglen includes nul terminator.
+    glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &loglen);
+    if (loglen > 1) {
+        char *log = malloc(loglen);
+        if (log == NULL) {
+            die("No memory");
+        }
+        glGetProgramInfoLog(prog, loglen, NULL, log);
+        log[loglen - 1] = '\0';
+        fputs("-- Shader linking log --\n", stderr);
+        fputs(log, stderr);
+        free(log);
+    }
+    if (!status) {
+        die("Shader linking failed.");
+    }
+    return prog;
 }
 
 int main(int argc, char **argv) {
@@ -54,11 +153,34 @@ int main(int argc, char **argv) {
     fprintf(stderr, "GL_VENDOR: %s\n", glGetString(GL_VENDOR));
     fprintf(stderr, "GL_RENDERER: %s\n", glGetString(GL_RENDERER));
 
+    GLuint prog = load_shaders();
+    GLuint arr;
+    GLuint buf;
+
+    glGenVertexArrays(1, &arr);
+    glBindVertexArray(arr);
+    glGenBuffers(1, &buf);
+    glBindBuffer(GL_ARRAY_BUFFER, buf);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6,
+                 (const float[][2]){
+                     {-0.6f, -0.8f},
+                     {0.6f, -0.8f},
+                     {0.0f, 0.8f},
+                 },
+                 GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     while (!glfwWindowShouldClose(window)) {
         double time = glfwGetTime();
         float color = fmod(time, 1.0);
         glClearColor(color, color, color, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(prog);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
